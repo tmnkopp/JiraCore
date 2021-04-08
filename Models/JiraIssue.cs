4a0d687006894759ac074ca7bc02576fcf96bcc8
Tim Kopp <timkopp@gmail.com>
2021-04-07 20:58:56 -0400

    updated formatting

---
+++ Models/JiraIssue.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JiraCore.Models
+{
+    public class JiraIssue
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public string issuekey { get; set; }
+        public string epiclink { get; set; }
+        public string section { get; set; }
+        public string epic  { get; set; }
+        public string title { get; set; }
+        public string link { get; set; }
+        public string labels { get; set; }
+        public string version { get; set; }
+        public string summary { get; set; }
+        public string description { get; set; }
+        public string desc { get; set; }
+        public string content { get; set; }
+    }
+}
